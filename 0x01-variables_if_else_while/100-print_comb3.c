a257362a3ed8c07f3e32c8c565e53fac42251ea0
TheNewJosh <oloshojodamilare@gmail.com>
2023-03-17 21:04:17 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,22 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i + '0');
-		if (i != 9)
+		for (j = 1; j < 10; j++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (i < j && i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
