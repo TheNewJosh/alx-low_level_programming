c59e9db0c885eaa0a326d67dc4156685f2eba6a3
TheNewJosh <oloshojodamilare@gmail.com>
2023-03-17 18:53:16 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: This return zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
