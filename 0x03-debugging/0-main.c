5fed67cdf3acf3260c90929d61164ecf563335ed
TheNewJosh <oloshojodamilare@gmail.com>
2023-03-23 10:53:54 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - This is the main function
+ *
+ * Return: this return zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
